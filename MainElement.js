702b278ea33849bff84ab56dfc8c8ff0185f828a
jorenbroekema <joren.broekema@gmail.com>
2021-06-20 11:11:13 +0200

    chore: initial setup

---
+++ MainElement.js
@@ -0,0 +1,1 @@
+export class MainElement extends HTMLElement {}
---
+++ index.js
@@ -0,0 +1,5 @@
+import { MainElement } from './MainElement.js';
+import { SuspenseElement } from './SuspenseElement.js';
+
+customElements.define('main-element', MainElement);
+customElements.define('suspense-element', SuspenseElement);
