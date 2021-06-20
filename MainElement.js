646e38a3bda9d97dd03196716372443f8d3acbc8
jorenbroekema <joren.broekema@gmail.com>
2021-06-20 12:11:04 +0200

    feat: implement suspense element POC

--- MainElement.js
+++ MainElement.js
@@ -1,1 +1,37 @@
-export class MainElement extends HTMLElement {}
+export class MainElement extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    // Set property to promise initially
+    this.list = new Promise((resolve /* , reject */) =>
+      setTimeout(() => {
+        resolve(['foo', 'bar', 'qux']);
+        // reject(); // <-- if you want to see the error fallback, make this suspense reject
+      }, 1000),
+    );
+  }
+
+  // Tag it as a suspense
+  get suspenses() {
+    return { list: this.list };
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <h1>Hello, World!</h1>
+      ${
+        // Assume it is resolved here and set to its result
+        this.list
+          ? this.list
+              .map(
+                (item) => `
+          <li>${item}</li>
+        `,
+              )
+              .reduce((acc, item) => acc.concat(item), '')
+          : ''
+      }
+    `;
+  }
+}
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import { MainElement } from './MainElement.js';
 import { SuspenseElement } from './SuspenseElement.js';
 
-customElements.define('main-element', MainElement);
 customElements.define('suspense-element', SuspenseElement);
+
+// Mocking a custom element which takes a bit of time to get defined on the
+// custom elements registry
+setTimeout(async () => {
+  const { MainElement } = await import('./MainElement.js');
+  customElements.define('main-element', MainElement);
+}, 500);
